2e6f58fefa0bd0e720397e662bdca719139490cb
chilux00 <chiingzhang@gmail.com>
2025-10-09 11:08:50 -0700

    clean up ode code

--- ode for LV.R
+++ ode for LV.R
@@ -1,4 +1,4 @@
-install.packages(deSolve)
+#install.packages(deSolve)
 library(deSolve)
 
 #predator prey L-V
@@ -21,11 +21,24 @@ Lot_Vol <- function (t, y, parms) {
     list(c(dX, dY))
   }) }
 
-times <- seq(from = 0, to = 100, by = 0.01) #this code sets the time interval population size will be sampled
-out   <- ode(y = yini, times = times, func = Lot_Vol, parms = NULL) #this code runs the model
+times <- seq(from = 0, 
+             to = 100, 
+             by = 0.01) #this code sets the time interval population size will be sampled
+out <- ode(y = yini, 
+           times = times, 
+           func = Lot_Vol, 
+           parms = NULL) #this code runs the model
 
 head(out) #here you can see the data frame created by the model
 
 #the code below plots the data from the data frame
-plot(y=out[, "X"], x = out[, "time"], type = 'l', col = "blue", xlab = "Time", ylab = "Animals (#)")
-lines(y=out[, "Y"], x = out[, "time"], type = 'l', col = "red")
+plot(y=out[, "X"], 
+     x = out[, "time"], 
+     type = 'l', 
+     col = "blue", 
+     xlab = "Time", 
+     ylab = "Animals (#)")
+lines(y=out[, "Y"], 
+      x = out[, "time"], 
+      type = 'l', 
+      col = "red")
